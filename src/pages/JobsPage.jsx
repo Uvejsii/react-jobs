d443662c53c126a6eeb4ca217125e0b03c4bb4ef
Uvejsii <136826106+Uvejsii@users.noreply.github.com>
2024-04-18 23:03:26 +0200

    fixed jobs listing based on Pages

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,8 +1,10 @@
+import JobListings from "../components/JobListings.jsx";
+
 const JobsPage = () => {
     return (
-        <>
-            JobsPage
-        </>
+        <section className="bg-blue-50 px-4 py-6">
+            <JobListings />
+        </section>
     )
 }
 export default JobsPage;
